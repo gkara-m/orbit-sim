de65c2f1779edd3c364d2ecf04ad4ea46a6827b8
gk <gwenkara@proton.me>
2026-04-28 22:13:58 +0100

    feat: add for loop

--- source/main.cpp
+++ source/main.cpp
@@ -51,27 +51,30 @@ auto get_acceleration_vector(const double mass, const std::array<double, 2>& for
 }
 
 auto main() -> int {
+  double time_interval { 0.001 };
 
-  Body default0 {0, 1e12, {-1, 0}, {0, 5} };
-  Body default1 {1, 1e12, {1, 0}, {0, -5} };
+  Body default0 {0, 1e4, {-1, 0}, {0, 1000} };
+  Body default1 {1, 1e4, {1, 0}, {0, -1000} };
   std::cout << "initial-positions: 0[" << default0.position[0] << ", " << default0.position[1] << "], 1[" << default1.position[0] << ", " << default1.position[1] << "].";
+  
+  for (int i = 0; i < 100; i++) {
+    double distance { get_distance(default0.position, default1.position) };
+    double force_scalar { get_force_scalar(default0.mass, default1.mass, distance)};
+    std::array<double, 2> unit_direction { get_unit_direction(default0.position, default1.position, distance) };
+    std::array<double, 2> force_vector_def0 { get_force_vector(force_scalar, unit_direction) };
+    std::array<double, 2> force_vector_def1 { -force_vector_def0[0], -force_vector_def0[1] }; // TODO use operator overloading to replace this
+    std::array<double, 2> acceleration_def0 { get_acceleration_vector(default0.mass, force_vector_def0) };
+    std::array<double, 2> acceleration_def1 { get_acceleration_vector(default1.mass, force_vector_def1) };
 
-  double distance { get_distance(default0.position, default1.position) };
-  double force_scalar { get_force_scalar(default0.mass, default1.mass, distance)};
-  std::array<double, 2> unit_direction { get_unit_direction(default0.position, default1.position, distance) };
-  std::array<double, 2> force_vector_def0 { get_force_vector(force_scalar, unit_direction) };
-  std::array<double, 2> force_vector_def1 { -force_vector_def0[0], -force_vector_def0[1] }; // TODO use operator overloading to replace this
-  std::array<double, 2> acceleration_def0 { get_acceleration_vector(default0.mass, force_vector_def0) };
-  std::array<double, 2> acceleration_def1 { get_acceleration_vector(default1.mass, force_vector_def1) };
+    std::array<double, 2> velocity_def0 { acceleration_def0[0] * time_interval, acceleration_def0[1] * time_interval };
+    std::array<double, 2> velocity_def1 { acceleration_def1[0] * time_interval, acceleration_def1[1] * time_interval };
+    std::array<double, 2> position_def0 { velocity_def0[0] * time_interval, velocity_def0[1] * time_interval };
+    std::array<double, 2> position_def1 { velocity_def1[0] * time_interval, velocity_def1[1] * time_interval };
+    default0.position = position_def0;
+    default1.position = position_def1;
 
-  double time_interval { 0.01 };
-
-  std::array<double, 2> position_def0 { default0.position[0] + acceleration_def0[0] * time_interval * time_interval, default0.position[1] + acceleration_def0[1] * time_interval * time_interval };
-  std::array<double, 2> position_def1 { default1.position[0] + acceleration_def1[0] * time_interval * time_interval, default1.position[1] + acceleration_def1[1] * time_interval * time_interval };
-  default0.position = position_def0;
-  default1.position = position_def1;
-
-  std::cout << "\n position at interval1: 0[" << default0.position[0] << ", " << default0.position[1] << "], 1[" << default1.position[0] << ", " << default1.position[1] << "].";
+    std::cout << "\n position at interval1: 0[" << default0.position[0] << ", " << default0.position[1] << "], 1[" << default1.position[0] << ", " << default1.position[1] << "].";
+  }
 
 
   return 0;
