5347c2af43b3a87dda2b36e5597cb4aa087cc2aa
gk <gwenkara@proton.me>
2026-05-02 15:17:07 +0100

    feat: start gui maximized

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import r from "raylib";
 
 const confPath: string = "data/config.json";
 const orbitSimPath: string = "../build/Release/orbit_sim.node";
+const bgColour: r.Color = {r: 30, g: 30, b: 30, a: 255};
 
 interface Settings {
   g: number;
@@ -20,6 +21,7 @@ interface Body {
 }
 
 interface UISettings {
+  // 0 = no gui, 1 = raylib gui
   gui: number;
 }
 
@@ -29,14 +31,31 @@ interface Config {
   uiSettings: UISettings;
 }
 
+function runRaylibGUI() {
+  r.SetConfigFlags(r.FLAG_WINDOW_RESIZABLE);
+  let width = r.GetScreenWidth();
+  let height = r.GetScreenHeight();
+  r.InitWindow(width/2, height/2, "Orbit Sim");
+  r.MaximizeWindow();
+  r.SetTargetFPS(60);
+  while (r.WindowShouldClose() == false) {
+    r.BeginDrawing();
+    r.ClearBackground(bgColour);
+    r.EndDrawing();
+  };
+}
 
 const jsonString = fs.readFileSync(confPath, "utf8");
 const data: Config = JSON.parse(jsonString);
 
-if (data.uiSettings.gui == 0) {
-  r.InitWindow(800, 800, "Orbit Sim");
+if (data.uiSettings.gui == 1) {
+  runRaylibGUI();
 }
 
 const require = createRequire(import.meta.url)
 const orbitSim = require(orbitSimPath);
 orbitSim.start(data);
+
+if (data.uiSettings.gui == 1) {
+  r.CloseWindow();
+};
